e8400ca5fee4c4692aa2504595cc4743a6059fb1
Weffy61 <weffy@mail.ru>
2025-08-06 19:55:42 +0300

    Add notifications

---
+++ notifications.py
@@ -0,0 +1,18 @@
+from aiogram import Bot
+from aiogram.enums import ParseMode
+from aiogram.types import FSInputFile
+from config import BOT_TOKEN, USER_ID
+
+bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
+
+
+async def send_start_message():
+    await bot.send_message(USER_ID, "🚀 Парсинг начат")
+
+
+async def send_end_message(filepath: str):
+    try:
+        file = FSInputFile(filepath)
+        await bot.send_document(USER_ID, file, caption="✅ Парсинг окончен")
+    except Exception as e:
+        await bot.send_message(USER_ID, f"⚠️ Ошибка при отправке файла: {e}")
