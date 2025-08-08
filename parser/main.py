039c3afe47dda6483bf36c408951412ba96adc3c
Weffy61 <weffy@mail.ru>
2025-08-08 21:48:20 +0300

    Remove hardcode - put count of workers in the config

--- parser/main.py
+++ parser/main.py
@@ -3,7 +3,8 @@ from asyncio import Queue
 
 from bs4 import BeautifulSoup
 
-from config import BASE_URL, OUTPUT_FILE
+from config import BASE_URL, OUTPUT_FILE, CATEGORY_WORKER_COUNT, YEAR_WORKER_COUNT, MODEL_WORKER_COUNT, \
+    PARTS_WORKER_COUNT, DETAILS_WORKER_COUNT
 from notifications import send_message, send_end_message, send_error_message
 from . import workers, core
 from .proxy_manager import ProxyManager
@@ -33,19 +34,19 @@ async def main(args):
             href = normalize_url(a.get('href'), BASE_URL)
             await core.enqueue_url(href, core.category_queue, 'category')
 
-        await collect(workers.category_worker, core.category_queue, 2)
+        await collect(workers.category_worker, core.category_queue, CATEGORY_WORKER_COUNT)
         await send_message('Собраны категории')
 
-        await collect(workers.year_worker, core.year_queue, 5)
+        await collect(workers.year_worker, core.year_queue, YEAR_WORKER_COUNT)
         await send_message('Собраны года')
 
-        await collect(workers.model_worker, core.model_queue, 5)
+        await collect(workers.model_worker, core.model_queue, MODEL_WORKER_COUNT)
         await send_message('Собраны модели')
 
-        await collect(workers.parts_worker, core.parts_queue, 5)
+        await collect(workers.parts_worker, core.parts_queue, PARTS_WORKER_COUNT)
         await send_message('Собраны части')
 
-        await collect(workers.details_worker, core.details_queue, 5)
+        await collect(workers.details_worker, core.details_queue, DETAILS_WORKER_COUNT)
         await send_message('Собраны детали')
 
         log('Parsing process completed successfully.')
