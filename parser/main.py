0ed8e5e84c30cc85fb443d602694cb08614302f6
Weffy61 <weffy@mail.ru>
2025-08-15 09:18:53 +0300

    Remove file sender

--- parser/main.py
+++ parser/main.py
@@ -35,22 +35,13 @@ async def main(args):
             href = normalize_url(a.get('href'), BASE_URL)
             await core.enqueue_url(href, core.category_queue, 'category')
 
+        await send_message('The parsing process has started')
         await collect(workers.category_worker, core.category_queue, CATEGORY_WORKER_COUNT)
-        await send_message('Собраны категории')
-
         await collect(workers.year_worker, core.year_queue, YEAR_WORKER_COUNT)
-        await send_message('Собраны года')
-
         await collect(workers.model_worker, core.model_queue, MODEL_WORKER_COUNT)
-        await send_message('Собраны модели')
-
         await collect(workers.parts_worker, core.parts_queue, PARTS_WORKER_COUNT)
-        await send_message('Собраны части')
-
         await collect(workers.details_worker, core.details_queue, DETAILS_WORKER_COUNT)
-        await send_message('Собраны детали')
-
+        await send_message('✅ Parsing process completed successfully')
         log('Parsing process completed successfully.')
-        await send_end_message(OUTPUT_FILE)
     except Exception as ex:
         await send_error_message(ex)
